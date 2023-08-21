c81ced6fb8ce56fa7c75750e5ce2dcc2a16fb510
Austin Cavanagh <austin.cavanagh.cs@gmail.com>
2023-08-20 23:04:33 -0700

    Version 1

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+// import { useState, useEffect } from "react"
+
+function App() {
+  return (
+    <>
+      <div className="window">
+        <div className="board">
+          <div className="row-roads">
+            <div className="road-up-right"></div>
+            <div className="road-down-right"></div>
+            <div className="road-up-right"></div>
+            <div className="road-down-right"></div>{" "}
+            <div className="road-up-right"></div>
+            <div className="road-down-right"></div>
+          </div>
+          <div className="row">
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+          </div>
+          <div className="row">
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+          </div>
+          <div className="row">
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+          </div>
+          <div className="row">
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+          </div>
+          <div className="row">
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+            <div className="hexagon"></div>
+            <div className="road"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
